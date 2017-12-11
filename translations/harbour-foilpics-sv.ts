4d75202a114b9e550145fa052c1e6b3d9f8d0900
Åke Engelbrektson <eson57@users.noreply.github.com>
2017-12-11 07:57:40 +0100

    Create harbour-foilpics-sv.ts
    Swedish translated if wanted. File is tested and found flawless.

---
+++ translations/harbour-foilpics-sv.ts
@@ -0,0 +1,351 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sv">
+<context>
+    <name></name>
+    <message id="foilpics-app_name">
+        <source>Foil Pics</source>
+        <extracomment>Application title</extracomment>
+        <translation>Foil Pics</translation>
+    </message>
+    <message id="foilpics-menu-details">
+        <source>Image details</source>
+        <extracomment>Generic menu item</extracomment>
+        <translation>Bildinformation</translation>
+    </message>
+    <message id="foilpics-menu-encrypt">
+        <source>Encrypt</source>
+        <extracomment>Generic menu item</extracomment>
+        <translation>Kryptera</translation>
+    </message>
+    <message id="foilpics-menu-decrypt">
+        <source>Decrypt</source>
+        <extracomment>Generic menu item</extracomment>
+        <translation>Avkryptera</translation>
+    </message>
+    <message id="foilpics-menu-delete">
+        <source>Delete</source>
+        <extracomment>Generic menu item</extracomment>
+        <translation>Ta bort</translation>
+    </message>
+    <message id="foilpics-pulley_menu-generate_key">
+        <source>Generate a new key</source>
+        <extracomment>Pulley menu item</extracomment>
+        <translation>Skapa ny nyckel</translation>
+    </message>
+    <message id="foilpics-pulley_menu-change_password">
+        <source>Change password</source>
+        <extracomment>Pulley menu item</extracomment>
+        <translation>Byt lösenord</translation>
+    </message>
+    <message id="foilpics-pulley_menu-decrypt_all">
+        <source>Decrypt all</source>
+        <extracomment>Pulley menu item</extracomment>
+        <translation>Avkryptera alla</translation>
+    </message>
+    <message id="foilpics-pulley_menu-remorse-decrypting_all">
+        <source>Decrypting all pictures</source>
+        <extracomment>Decrypting all pictures in 5 seconds</extracomment>
+        <translation>Avkrypterar alla bilder om 5 sekunder</translation>
+    </message>
+    <message id="foilpics-file_size-bytes">
+        <source>%n B</source>
+        <translation>%n B</translation>
+    </message>
+    <message id="foilpics-file_size-kilobytes">
+        <source>%1 kB</source>
+        <translation>%1 kB</translation>
+    </message>
+    <message id="foilpics-file_size-megabytes">
+        <source>%1 MB</source>
+        <translation>%1 MB</translation>
+    </message>
+    <message id="foilpics-file_size-gigabytes">
+        <source>%1 GB</source>
+        <translation>%1 GB</translation>
+    </message>
+    <message id="foilpics-file_size-terabytes">
+        <source>%1 TB</source>
+        <translation>%1 TB</translation>
+    </message>
+    <message id="foilpics-notification-generated_key">
+        <source>Generated new key</source>
+        <extracomment>Pop-up notification</extracomment>
+        <translation>Genererade ny nyckel</translation>
+    </message>
+    <message id="foilpics-notification-password_changed">
+        <source>Password changed</source>
+        <extracomment>Pop-up notification</extracomment>
+        <translation>Lösenord ändrat</translation>
+    </message>
+    <message id="foilpics-hint-swipe_left_to_gallery">
+        <source>Swipe left to access the picture gallery</source>
+        <extracomment>Left swipe hint text</extracomment>
+        <translation>Svep åt vänster för att nå bildgalleriet</translation>
+    </message>
+    <message id="foilpics-hint-swipe_right_to_encrypted">
+        <source>Encrypted pictures are moved there to the left</source>
+        <extracomment>Right swipe hint text</extracomment>
+        <translation>Krypterade bilder flyttas åt vänster</translation>
+    </message>
+    <message id="foilpics-hint-swipe_left_to_decrypted">
+        <source>Decrypted pictures are moved back to the gallery</source>
+        <extracomment>Left swipe hint text</extracomment>
+        <translation>Avkrypterade bilder flyttas tillbaka till galleriet</translation>
+    </message>
+    <message id="foilpics-generate_key_warning-title">
+        <source>Warning</source>
+        <extracomment>Title for the new key warning</extracomment>
+        <translation>Varning!</translation>
+    </message>
+    <message id="foilpics-generate_key_warning-text">
+        <source>You seem to have some encrypted files in the storage folder. Once you have generated a new key, you are going to lose access to those files. If you have forgotten your password, then keep in mind that most likely it&apos;s computationally easier to brute-force your password and recover the old key than to crack the new key.</source>
+        <extracomment>Warning shown prior to generating the new key</extracomment>
+        <translation>Du verkar ha några krypterade filer i lagringsmappen. När du väl har skapat en ny nyckel, kommer du att förlora åtkomsten till dessa filer. 
+Om du har glömt ditt lösenord, bör du veta att det troligen är datortekniskt enklare att hacka ditt lösenord (brute force) och återställa den gamla nyckeln, än att knäcka den nya nyckeln.</translation>
+    </message>
+    <message id="foilpics-generate_key_page-title">
+        <source>You are about to generate a new key</source>
+        <extracomment>Label text</extracomment>
+        <translation>Du är på väg att generera en ny nyckel</translation>
+    </message>
+    <message id="foilpics-generating_key_view-generating_new_key">
+        <source>Generating new key...</source>
+        <extracomment>Progress view label</extracomment>
+        <translation>Genererar ny nyckel...</translation>
+    </message>
+    <message id="foilpics-generate_key_view-label-key_needed">
+        <source>You need to generate the key and select the password before you can encrypt your pictures</source>
+        <extracomment>Label text</extracomment>
+        <translation>Du måste skapa en nyckel och ange ett lösenord, innan du kan kryptera dina bilder</translation>
+    </message>
+    <message id="foilpics-generate_key_view-label-key_size">
+        <source>Key size</source>
+        <extracomment>Combo box label</extracomment>
+        <translation>Nyckelstorlek</translation>
+    </message>
+    <message id="foilpics-generate_key_view-label-minimum_length" numerus="yes">
+        <source>Type at least %0 character(s)</source>
+        <extracomment>Password field label</extracomment>
+        <translation>
+            <numerusform>Skriv minst %0 tecken</numerusform>
+            <numerusform>Skriv minst %0 tecken</numerusform>
+        </translation>
+    </message>
+    <message id="foilpics-generate_key_view-button-generate_key">
+        <source>Generate key</source>
+        <extracomment>Button label</extracomment>
+        <translation>Skapa nyckel</translation>
+    </message>
+    <message id="foilpics-generate_key_view-button-generating_key">
+        <source>Generating...</source>
+        <extracomment>Button label</extracomment>
+        <translation>Genererar...</translation>
+    </message>
+    <message id="foilpics-enter_password_view-label-enter_password">
+        <source>Secret pictures are locked. Please enter your password</source>
+        <extracomment>Password prompt label</extracomment>
+        <translation>Hemliga bilder är låsta. Ange ditt lösenord</translation>
+    </message>
+    <message id="foilpics-enter_password_view-button-unlocking">
+        <source>Unlocking...</source>
+        <extracomment>Button label</extracomment>
+        <translation>Låser upp...</translation>
+    </message>
+    <message id="foilpics-enter_password_view-button-unlock">
+        <source>Unlock</source>
+        <extracomment>Button label</extracomment>
+        <translation>Lås upp</translation>
+    </message>
+    <message id="foilpics-change_password_page-label-enter_passwords">
+        <source>Please enter the current and the new password</source>
+        <extracomment>Password change prompt</extracomment>
+        <translation>Ange det aktuella och ett nytt lösenord</translation>
+    </message>
+    <message id="foilpics-change_password_page-text_field_label-current_password">
+        <source>Current password</source>
+        <extracomment>Placeholder and label for the current password prompt</extracomment>
+        <translation>Aktuellt lösenord</translation>
+    </message>
+    <message id="foilpics-change_password_page-text_field_label-new_password">
+        <source>New password</source>
+        <extracomment>Placeholder and label for the new password prompt</extracomment>
+        <translation>Nytt lösenord</translation>
+    </message>
+    <message id="foilpics-change_password_page-button-change_password">
+        <source>Change password</source>
+        <extracomment>Button label</extracomment>
+        <translation>Byt lösenord</translation>
+    </message>
+    <message id="foilpics-decrypting_view-unlocking">
+        <source>Unlocking...</source>
+        <extracomment>Progress view label</extracomment>
+        <translation>Låser upp...</translation>
+    </message>
+    <message id="foilpics-decrypting_view-decrypting">
+        <source>Decrypting...</source>
+        <extracomment>Progress view label</extracomment>
+        <translation>Avkrypterar...</translation>
+    </message>
+    <message id="foilpics-image_grid_view-remorse-deleting">
+        <source>Deleting</source>
+        <extracomment>Deleting image in 5 seconds</extracomment>
+        <translation>Tar bort</translation>
+    </message>
+    <message id="foilpics-image_grid_view-remorse-cancel_deletion">
+        <source>Cancel</source>
+        <extracomment>RemorseItem cancel help text</extracomment>
+        <translation>Avbryt</translation>
+    </message>
+    <message id="foilpics-gallery_grid-title">
+        <source>Photos</source>
+        <extracomment>Gallery grid title</extracomment>
+        <translation>Bilder</translation>
+    </message>
+    <message id="foilpics-gallery_view-placeholder-no_pictures">
+        <source>The photo gallery seems to be empty</source>
+        <extracomment>Placeholder text</extracomment>
+        <translation>Bildgalleriet verkar vara tomt</translation>
+    </message>
+    <message id="foilpics-gallery_fullscreen_view-header">
+        <source>Share</source>
+        <extracomment>Page header</extracomment>
+        <translation>Dela</translation>
+    </message>
+    <message id="foilpics-encrypted_grid-title">
+        <source>Encrypted</source>
+        <extracomment>Encrypted grid title</extracomment>
+        <translation>Krypterat</translation>
+    </message>
+    <message id="foilpics-thumbnail_image-loading_failed">
+        <source>Oops, can&apos;t display the thumbnail!</source>
+        <extracomment>Thumbnail Image loading failed</extracomment>
+        <translation>Hoppsan, kan inte visa miniatyren</translation>
+    </message>
+    <message id="foilpics-share_method_list-add_account">
+        <source>Add account</source>
+        <extracomment>Share list item</extracomment>
+        <translation>Lägg till konto</translation>
+    </message>
+    <message id="foilpics-encrypted_grid-remorse-cancel_decrypt">
+        <source>Cancel</source>
+        <extracomment>RemorseItem cancel help text</extracomment>
+        <translation>Avbryt</translation>
+    </message>
+    <message id="foilpics-encrypted_grid-placeholder-no_pictures_hint">
+        <source>Why not to encrypt something?</source>
+        <extracomment>Placeholder text with a hint</extracomment>
+        <translation>Varför inte kryptera något?</translation>
+    </message>
+    <message id="foilpics-encrypted_grid-placeholder-no_pictures">
+        <source>You don&apos;t have any encrypted pictures</source>
+        <extracomment>Placeholder text</extracomment>
+        <translation>Du har inga krypterade bilder</translation>
+    </message>
+    <message id="foilpics-encrypted_grid-remorse-decrypting">
+        <source>Decrypting</source>
+        <extracomment>Decrypting image in 5 seconds</extracomment>
+        <translation>Avkrypterar</translation>
+    </message>
+    <message id="foilpics-confirm_password_page-info_label">
+        <source>Please type in your new password one more time</source>
+        <extracomment>Password confirmation label</extracomment>
+        <translation>Ange ditt lösenord en gång till</translation>
+    </message>
+    <message id="foilpics-confirm_password_page-description">
+        <source>Make sure you don&apos;t forget your password. It&apos;s impossible to either recover it or to access the encrypted pictures without knowing it. Better take it seriously.</source>
+        <extracomment>Password confirmation description</extracomment>
+        <translation>Se till att du inte glömmer ditt lösenord. Det går varken att återställa lösenordet, eller att komma åt de krypterade bilderna om du glömmer det. Ta det på allvar!</translation>
+    </message>
+    <message id="foilpics-confirm_password_page-text_field_placeholder-new_password">
+        <source>New password again</source>
+        <extracomment>Placeholder for the password confirmation prompt</extracomment>
+        <translation>Nytt lösenord igen</translation>
+    </message>
+    <message id="foilpics-confirm_password_page-text_field_label-new_password">
+        <source>New password</source>
+        <extracomment>Label for the password confirmation prompt</extracomment>
+        <translation>Nytt lösenord</translation>
+    </message>
+    <message id="foilpics-confirm_password_page-button-confirm">
+        <source>Confirm</source>
+        <extracomment>Button label (confirm password)</extracomment>
+        <translation>Bekräfta</translation>
+    </message>
+    <message id="foilpics-details-header">
+        <source>Details</source>
+        <extracomment>Page header</extracomment>
+        <translation>Information</translation>
+    </message>
+    <message id="foilpics-details-file_name-label">
+        <source>File name</source>
+        <extracomment>Details label</extracomment>
+        <translation>Filnamn</translation>
+    </message>
+    <message id="foilpics-details-file_size-label">
+        <source>File size</source>
+        <extracomment>Details label</extracomment>
+        <translation>Filstorlek</translation>
+    </message>
+    <message id="foilpics-details-original_file_size-label">
+        <source>Original file size</source>
+        <extracomment>Details label</extracomment>
+        <translation>Ursprunglig filstorlek</translation>
+    </message>
+    <message id="foilpics-details-encrypted_file_size-label">
+        <source>Encrypted file size</source>
+        <extracomment>Details label</extracomment>
+        <translation>Filstorlek krypterad</translation>
+    </message>
+    <message id="foilpics-details-mime_type-label">
+        <source>Type</source>
+        <extracomment>Details label</extracomment>
+        <translation>Typ</translation>
+    </message>
+    <message id="foilpics-details-width-label">
+        <source>Width</source>
+        <extracomment>Details label</extracomment>
+        <translation>Bredd</translation>
+    </message>
+    <message id="foilpics-details-height-label">
+        <source>Height</source>
+        <extracomment>Details label</extracomment>
+        <translation>Höjd</translation>
+    </message>
+    <message id="foilpics-details-date-label">
+        <source>Date</source>
+        <extracomment>Details label</extracomment>
+        <translation>Datum</translation>
+    </message>
+    <message id="foilpics-details-camera_manufacturer-label">
+        <source>Camera manufacturer</source>
+        <extracomment>Details label</extracomment>
+        <translation>Kameratillverkare</translation>
+    </message>
+    <message id="foilpics-details-camera_model-label">
+        <source>Camera model</source>
+        <extracomment>Details label</extracomment>
+        <translation>Kameramodell</translation>
+    </message>
+    <message id="foilpics-details-coordinates-label">
+        <source>Coordinates</source>
+        <extracomment>Details label</extracomment>
+        <translation>Koordinater</translation>
+    </message>
+    <message id="foilpics-details-coordinates-value">
+        <source>Latitude %1, Longitude %2, Altitude %3</source>
+        <extracomment>Coordinates</extracomment>
+        <translation>Latitud %1, Longitud %2, Altitud %3</translation>
+    </message>
+    <message id="foilpics-details-section_header-editable">
+        <source>Editable details</source>
+        <extracomment>Section header for editable details</extracomment>
+        <translation>Redigerbar information</translation>
+    </message>
+    <message id="foilpics-details-image_title-label">
+        <source>Title</source>
+        <extracomment>Details label</extracomment>
+        <translation>Namn</translation>
+    </message>
+</context>
+</TS>
