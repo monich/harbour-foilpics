11aa56dc897ab4fd67caab54ee1b5b127a4d2f94
Slava Monich <slava@monich.com>
2021-11-24 21:18:02 +0200

    [foilpics] Delete multiple local files more accurately
    First build the list of URLs and then delete them one by one.
    This makes sure that the model doesn't get modified while we are
    building the list and we don't accidentally delete a wrong file.

--- src/FoilPicsFileUtil.cpp
+++ src/FoilPicsFileUtil.cpp
@@ -1,6 +1,6 @@
 /*
- * Copyright (C) 2017-2019 Jolla Ltd.
- * Copyright (C) 2017-2019 Slava Monich <slava@monich.com>
+ * Copyright (C) 2017-2021 Jolla Ltd.
+ * Copyright (C) 2017-2021 Slava Monich <slava@monich.com>
  *
  * You may use this file under the terms of the BSD license as follows:
  *
@@ -284,6 +284,7 @@ bool FoilPicsFileUtil::deleteLocalFile(QUrl aUrl)
 void FoilPicsFileUtil::deleteLocalFilesFromModel(QObject* aModel,
     QString aRole, QList<int> aRows)
 {
+    HDEBUG(aRows);
     const int n = aRows.count();
     if (n > 0) {
         QAbstractItemModel* model = qobject_cast<QAbstractItemModel*>(aModel);
@@ -291,17 +292,25 @@ void FoilPicsFileUtil::deleteLocalFilesFromModel(QObject* aModel,
         if (model) {
             const int role = FoilPicsRole::find(model, aRole);
             if (role >= 0) {
+                int i;
                 const int column = 0;
                 const int rowCount = model->rowCount();
-                for (int i = 0; i < n; i++) {
+                QList<QUrl> urls;
+                urls.reserve(n);
+                for (i = 0; i < n; i++) {
                     const int row = aRows.at(i);
                     if (row >= 0 && row < rowCount) {
                         QModelIndex index(model->index(row, column));
                         QUrl url(model->data(index, role).toUrl());
                         if (url.isValid()) {
-                            deleteLocalFile(url);
+                            HDEBUG(url);
+                            urls.append(url);
                         }
                     }
+                }
+                const int urlCount = urls.count();
+                for (i = 0; i < urlCount; i++) {
+                    deleteLocalFile(urls.at(i));
                 }
             }
         }
